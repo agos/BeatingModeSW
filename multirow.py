90d0586366459570c79f1817173603179e39eec0
Agostino Carandente <aragost@gmail.com>
2011-03-28 12:37:08 +0200

    Spostate prima tutte le scritture

--- multirow.py
+++ multirow.py
@@ -21,16 +21,17 @@ beatingimage = BeatingImage(path="dati/samp6.dat", repetitions=90, shutter_frequ
 print beatingimage.reconstructed_on.shape
 
 savetxt("out/reconstructed_on.dat", beatingimage.reconstructed_on, fmt="%3.4f0")
+savetxt("out/reconstructed_off.dat", beatingimage.reconstructed_off, fmt="%3.4f0")
+savetxt("out/enhancement_ratios.dat", beatingimage.ratios, fmt="%3.4f0")
+
 pylab.subplot(2,2,1)
 reconstructed_on_image = pylab.imshow(beatingimage.reconstructed_on, cmap=my_color_map)
 reconstructed_on_image.set_interpolation('nearest')
 
-savetxt("out/reconstructed_off.dat", beatingimage.reconstructed_off, fmt="%3.4f0")
 pylab.subplot(2,2,2)
 reconstructed_off_image = pylab.imshow(beatingimage.reconstructed_off, cmap=my_color_map)
 reconstructed_off_image.set_interpolation('nearest')
 
-savetxt("out/enhancement_ratios.dat", beatingimage.ratios, fmt="%3.4f0")
 pylab.subplot(2,2,3)
 enhancement_ratios_image = pylab.imshow(beatingimage.ratios, cmap=my_color_map)
 enhancement_ratios_image.set_interpolation('nearest')
