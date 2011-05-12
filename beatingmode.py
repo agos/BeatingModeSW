bc71182da0ff80298f87a40580818070d6d6cfaa
Agostino Carandente <aragost@gmail.com>
2011-05-12 16:45:16 +0200

    Riordinate un po' di cose

--- beatingmode.py
+++ beatingmode.py
@@ -293,26 +293,30 @@ class BeatingImage(object):
 
 
 if __name__ == '__main__':
-    beatingimage = BeatingImage(path="dati/generated.dat")
-    # beatingimage = BeatingImage(path="dati/samp6.dat")
+    bimg = BeatingImage(path="dati/generated.dat")
+    # bimg = BeatingImage(path="dati/samp6.dat")
 
-    print("Immagine ricostruita: {0}".format(beatingimage.reconstructed_on.shape))
+    print("Immagine ricostruita: {0}".format(bimg.reconstructed_on.shape))
 
-    savetxt("out/reconstructed_on.dat", beatingimage.reconstructed_on, fmt="%3.4f0")
-    savetxt("out/reconstructed_off.dat", beatingimage.reconstructed_off, fmt="%3.4f0")
-    savetxt("out/enhancement_ratios.dat", beatingimage.ratios, fmt="%3.4f0")
+    rec_on = bimg.reconstructed_on
+    rec_off = bimg.reconstructed_off
+    ratios = bimg.ratios
+
+    savetxt("out/reconstructed_on.dat", rec_on, fmt="%3.4f0")
+    savetxt("out/reconstructed_off.dat", rec_off, fmt="%3.4f0")
+    savetxt("out/enhancement_ratios.dat", ratios, fmt="%3.4f0")
 
     pylab.subplot(2, 2, 1)
-    reconstructed_on_image = pylab.imshow(beatingimage.reconstructed_on, cmap=rate_color_map)
+    reconstructed_on_image = pylab.imshow(rec_on, cmap=rate_color_map)
     reconstructed_on_image.set_interpolation('nearest')
 
     pylab.subplot(2, 2, 2)
-    reconstructed_off_image = pylab.imshow(beatingimage.reconstructed_off, cmap=rate_color_map)
+    reconstructed_off_image = pylab.imshow(rec_off, cmap=rate_color_map)
     reconstructed_off_image.set_interpolation('nearest')
 
     pylab.subplot(2, 2, 3)
-    enhancement_ratios_image = pylab.imshow(beatingimage.ratios, cmap=ratio_color_map)
-    pylab.colorbar()
+    enhancement_ratios_image = pylab.imshow(ratios, cmap=ratio_color_map)
     enhancement_ratios_image.set_interpolation('nearest')
+    pylab.colorbar()
 
     pylab.show()
